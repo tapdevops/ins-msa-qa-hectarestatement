efacf7668eaa8e78520ea6234e86e5a3e142a0b2
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2018-11-13 15:44:58 +0700

    Ferdinand

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ module.exports = {
 	app_port: process.env.PORT || 3003,
 	app_name: 'Microservices Hectare Statement',
 	url: {
-		sync_list: 'http://localhost:3001/sync/list'
+		sync_list: 'http://149.129.242.205:3001/sync/list'
+		//sync_list: 'http://localhost:3001/sync/list'
 	}
 }
