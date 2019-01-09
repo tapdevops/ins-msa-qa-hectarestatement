422ae08837f31fac2508bb8066df84f2254b2bf8
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-09 14:44:41 +0700

    Ferdinand

--- app/controllers/comp.js
+++ app/controllers/comp.js
@@ -566,9 +566,9 @@ exports.update = ( req, res ) => {
 				status: true,
 				message: 'Data Sync tanggal ' + date.convert( req.params.start_date, 'YYYY-MM-DD' ) + ' s/d ' + date.convert( req.params.end_date, 'YYYY-MM-DD' ),
 				data: {
-					"insert": temp_insert,
-					"update": temp_update,
-					"delete": temp_delete
+					"hapus": temp_delete,
+					"simpan": temp_insert,
+					"ubah": temp_update
 				}
 			});
 		} ).catch( err => {
