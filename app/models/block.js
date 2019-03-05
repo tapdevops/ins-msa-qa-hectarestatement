e89f4b404952798f9b50e1da248ec24f5108fd4b
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-03-05 14:34:55 +0700

    Ferdinand

--- app/models/block.js
+++ app/models/block.js
@@ -6,6 +6,15 @@ const BlockSchema = mongoose.Schema( {
 	COMP_CODE: String,
 	EST_CODE: String,
 	WERKS: String,
+	JUMLAH_TPH: {
+		type: Number,
+		get: v => Math.floor( v ),
+		set: v => Math.floor( v ),
+		alias: 'i',
+		default: function() {
+			return null;
+		}
+	},
 	AFD_CODE: String,
 	BLOCK_CODE: String,
 	BLOCK_NAME: String,
