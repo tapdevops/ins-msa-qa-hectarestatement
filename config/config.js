79edf0e1b7887e13ab7a570a13aebe836d4e062b
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-04-10 13:56:02 +0700

    Ferdinand

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
 	*/
 	port: process.env.PORT || 3009,
 	name: 'Microservice Hectare Statement',
-	env: 'production', // production, qa, development
+	env: 'development', // production, qa, development
 
 	/*
 	|--------------------------------------------------------------------------
