be99909b5ee7df95c45b36f93eb9c0d281c62314
Ferd Shinoda <mac@Macs-MacBook-Pro-2.local>
2019-05-29 14:48:43 +0700

    Ferdinand

--- config/app.js
+++ config/app.js
@@ -10,9 +10,13 @@
 		| App Config
 		|--------------------------------------------------------------------------
 		*/
-			port: process.env.PORT || 3009,
 			name: 'Microservice Inspection',
 			env: 'development', // production, qa, development
+			port: {
+				development: process.env.PORT || 4009,
+				qa: process.env.PORT || 5009,
+				production: process.env.PORT || 3009,
+			},
 
 		/*
 		|--------------------------------------------------------------------------
@@ -28,31 +32,31 @@
 		| URL
 		|--------------------------------------------------------------------------
 		*/
-		url: {},
+			url: {},
 		
 		/*
 		|--------------------------------------------------------------------------
 		| Error Message
 		|--------------------------------------------------------------------------
 		*/
-		error_message: {
-			invalid_token: 'Token expired! ',
-			invalid_request: 'Invalid Request! ',
-			create_200: 'Success! ',
-			create_403: 'Forbidden ',
-			create_404: 'Error! Data gagal diproses. ',
-			create_500: 'Error! Terjadi kesalahan dalam pembuatan data ',
-			find_200: 'Success! ',
-			find_403: 'Forbidden ',
-			find_404: 'Error! Tidak ada data yang ditemukan ',
-			find_500: 'Error! Terjadi kesalahan dalam penampilan data ',
-			put_200: 'Success! ',
-			put_403: 'Forbidden ',
-			put_404: 'Error! Data gagal diupdate ',
-			put_500: 'Error! Terjadi kesalahan dalam perubahan data ',
-			delete_200: 'Success! ',
-			delete_403: 'Forbidden ',
-			delete_404: 'Error! Data gagal dihapus ',
-			delete_500: 'Error! Terjadi kesalahan dalam penghapusan data ',
-		}
+			error_message: {
+				invalid_token: 'Token expired! ',
+				invalid_request: 'Invalid Request! ',
+				create_200: 'Success! ',
+				create_403: 'Forbidden ',
+				create_404: 'Error! Data gagal diproses. ',
+				create_500: 'Error! Terjadi kesalahan dalam pembuatan data ',
+				find_200: 'Success! ',
+				find_403: 'Forbidden ',
+				find_404: 'Error! Tidak ada data yang ditemukan ',
+				find_500: 'Error! Terjadi kesalahan dalam penampilan data ',
+				put_200: 'Success! ',
+				put_403: 'Forbidden ',
+				put_404: 'Error! Data gagal diupdate ',
+				put_500: 'Error! Terjadi kesalahan dalam perubahan data ',
+				delete_200: 'Success! ',
+				delete_403: 'Forbidden ',
+				delete_404: 'Error! Data gagal dihapus ',
+				delete_500: 'Error! Terjadi kesalahan dalam penghapusan data ',
+			}
 	}
--- server.js
+++ server.js
@@ -3,51 +3,58 @@
 | Global APP Init
 |--------------------------------------------------------------------------
 */
-	global._directory_base = __dirname;
-	global.config = {};
-		  config.app = require( './config/app.js' );
-		  config.database = require( './config/database.js' )[config.app.env];
+global._directory_base = __dirname;
+global.config = {};
+	  config.app = require( './config/app.js' );
+	  config.database = require( './config/database.js' )[config.app.env];
 
 /*
 |--------------------------------------------------------------------------
 | APP Setup
 |--------------------------------------------------------------------------
 */
-	// Node Modules
-	const bodyParser = require( 'body-parser' );
-	const express = require( 'express' );
-	const mongoose = require( 'mongoose' );
+// Node Modules
+const body_parser = require( 'body-parser' );
+const express = require( 'express' );
+const mongoose = require( 'mongoose' );
 
-	// Primary Variable
-	const app = express();
+// Primary Variable
+const app = express();
 
 /*
 |--------------------------------------------------------------------------
 | APP Init
 |--------------------------------------------------------------------------
 */
-	// Parse request of content-type - application/x-www-form-urlencoded
-	app.use( bodyParser.urlencoded( { extended: false } ) );
+// Parse request of content-type - application/x-www-form-urlencoded
+app.use( body_parser.urlencoded( { extended: false } ) );
 
-	// Parse request of content-type - application/json
-	app.use( bodyParser.json() );
+// Parse request of content-type - application/json
+app.use( body_parser.json() );
 
-	// Setup Database
-	mongoose.Promise = global.Promise;
-	mongoose.connect( config.database.url, {
-		useNewUrlParser: true,
-		ssl: config.database.ssl
-	} ).then( () => {
-		console.log( 'Successfully connected to the Database (' + config.database.url + ')' );
-	} ).catch( err => {
-		console.log( 'Could not connect to the Database. Exiting application.' )
-	} );
+// Setup Database
+mongoose.Promise = global.Promise;
+mongoose.connect( config.database.url, {
+	useNewUrlParser: true,
+	ssl: config.database.ssl
+} ).then( () => {
+	console.log( "Database :" );
+	console.log( "\tStatus \t\t: Connected" );
+	console.log( "\tMongoDB URL \t: " + config.database.url + " (" + config.app.env + ")" );
+} ).catch( err => {
+	console.log( "Database :" );
+	console.log( "\tDatabase Status : Not Connected" );
+	console.log( "\tMongoDB URL \t: " + config.database.url + " (" + config.app.env + ")" );
+} );
 
-	// Server Running Message
-	app.listen( config.app.port, () => {
-		console.log( 'Server ' + config.app.name + ' Berjalan di port ' + config.app.port );
-	} );
+// Server Running Message
+app.listen( parseInt( config.app.port[config.app.env] ), () => {
+	console.log( "Server :" );
+	console.log( "\tStatus \t\t: OK" );
+	console.log( "\tService \t: " + config.app.name + " (" + config.app.env + ")" );
+	console.log( "\tPort \t\t: " + config.app.port[config.app.env] );
+} );
 
-	// Routing
-	require( './routes/api.js' )( app );
-	module.exports = app;
+// Routing
+require( './routes/api.js' )( app );
+module.exports = app;
